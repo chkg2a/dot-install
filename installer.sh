241d89192d7ad3578fe1a751ab57e3f7b0994838
chkg2a <xshalltearvamps@gmail.com>
2024-03-11 23:16:16 +0530

    remove oh-my-zsh and replace with zap

--- installer.sh
+++ installer.sh
@@ -1,10 +1,13 @@
-sudo pacman -S --needed git base-devel networkmanager xorg xorg-server xorg-xinit pipewire xwallpaper firefox stow xcompmgr ttf-jetbrains-mono-nerd upower zsh btop feh zoxide unzip python3 npm
+sudo pacman -S --needed git base-devel networkmanager xorg xorg-server xorg-xinit pipewire xwallpaper firefox stow xcompmgr ttf-jetbrains-mono-nerd upower zsh btop feh zoxide unzip python3 npm ncdu
 
 cd $HOME
 git clone https://github.com/chkg2a/dotfiles.git dotfiles
 cd $HOME/dotfiles/
 git submodule update --init --recursive
-cp -r .config .local .xinitrc .zprofile .zshrc .oh-my-zsh $HOME
+
+# Installing zap, zsh plugin manager
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+cp -r .config .local .xinitrc .zprofile .zshrc $HOME
 chsh /bin/zsh
 # stow --adopt .
 
